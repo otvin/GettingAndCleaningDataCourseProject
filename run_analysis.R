00205ad11247d976e893276da0aa0b5c7d286f47
Vin <otvin@users.noreply.github.com>
2015-05-24 16:02:58 -0400

    improve test for utils and dplyr
    Was getting error after run_analysis() was called when dplyr was not installed, so only run_analysis() if both utils and dplyr are available.

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,10 @@
-library(utils)
-library(dplyr)
-
 run_analysis <- function () {
-
+  
   #start with testing the directory and file exist...
   if (file.exists("UCI HAR Dataset") == FALSE) {
     stop("Dataset not found.  Dataset must be present in working directory.  See README.md for details.")
   }
-    
+  
   test_data <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
   test_subjects <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
   test_activities <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
@@ -43,85 +40,95 @@ run_analysis <- function () {
                                505,506,518,519,531,532,544,545)]
   
   colnames(trimmed_set) <- c("subject", "activity",
-  "tBodyAcc-mean()-X",
-  "tBodyAcc-mean()-Y",
-  "tBodyAcc-mean()-Z",
-  "tBodyAcc-std()-X",
-  "tBodyAcc-std()-Y",
-  "tBodyAcc-std()-Z",
-  "tGravityAcc-mean()-X",
-  "tGravityAcc-mean()-Y",
-  "tGravityAcc-mean()-Z",
-  "tGravityAcc-std()-X",
-  "tGravityAcc-std()-Y",
-  "tGravityAcc-std()-Z",
-  "tBodyAccJerk-mean()-X",
-  "tBodyAccJerk-mean()-Y",
-  "tBodyAccJerk-mean()-Z",
-  "tBodyAccJerk-std()-X",
-  "tBodyAccJerk-std()-Y",
-  "tBodyAccJerk-std()-Z",
-  "tBodyGyro-mean()-X",
-  "tBodyGyro-mean()-Y",
-  "tBodyGyro-mean()-Z",
-  "tBodyGyro-std()-X",
-  "tBodyGyro-std()-Y",
-  "tBodyGyro-std()-Z",
-  "tBodyGyroJerk-mean()-X",
-  "tBodyGyroJerk-mean()-Y",
-  "tBodyGyroJerk-mean()-Z",
-  "tBodyGyroJerk-std()-X",
-  "tBodyGyroJerk-std()-Y",
-  "tBodyGyroJerk-std()-Z",
-  "tBodyAccMag-mean()",
-  "tBodyAccMag-std()",
-  "tGravityAccMag-mean()",
-  "tGravityAccMag-std()",
-  "tBodyAccJerkMag-mean()",
-  "tBodyAccJerkMag-std()",
-  "tBodyGyroMag-mean()",
-  "tBodyGyroMag-std()",
-  "tBodyGyroJerkMag-mean()",
-  "tBodyGyroJerkMag-std()",
-  "fBodyAcc-mean()-X",
-  "fBodyAcc-mean()-Y",
-  "fBodyAcc-mean()-Z",
-  "fBodyAcc-std()-X",
-  "fBodyAcc-std()-Y",
-  "fBodyAcc-std()-Z",
-  "fBodyAccJerk-mean()-X",
-  "fBodyAccJerk-mean()-Y",
-  "fBodyAccJerk-mean()-Z",
-  "fBodyAccJerk-std()-X",
-  "fBodyAccJerk-std()-Y",
-  "fBodyAccJerk-std()-Z",
-  "fBodyGyro-mean()-X",
-  "fBodyGyro-mean()-Y",
-  "fBodyGyro-mean()-Z",
-  "fBodyGyro-std()-X",
-  "fBodyGyro-std()-Y",
-  "fBodyGyro-std()-Z",
-  "fBodyAccMag-mean()",
-  "fBodyAccMag-std()",
-  "fBodyBodyAccJerkMag-mean()",
-  "fBodyBodyAccJerkMag-std()",
-  "fBodyBodyGyroMag-mean()",
-  "fBodyBodyGyroMag-std()",
-  "fBodyBodyGyroJerkMag-mean()",
-  "fBodyBodyGyroJerkMag-std()"
+                             "tBodyAcc-mean()-X",
+                             "tBodyAcc-mean()-Y",
+                             "tBodyAcc-mean()-Z",
+                             "tBodyAcc-std()-X",
+                             "tBodyAcc-std()-Y",
+                             "tBodyAcc-std()-Z",
+                             "tGravityAcc-mean()-X",
+                             "tGravityAcc-mean()-Y",
+                             "tGravityAcc-mean()-Z",
+                             "tGravityAcc-std()-X",
+                             "tGravityAcc-std()-Y",
+                             "tGravityAcc-std()-Z",
+                             "tBodyAccJerk-mean()-X",
+                             "tBodyAccJerk-mean()-Y",
+                             "tBodyAccJerk-mean()-Z",
+                             "tBodyAccJerk-std()-X",
+                             "tBodyAccJerk-std()-Y",
+                             "tBodyAccJerk-std()-Z",
+                             "tBodyGyro-mean()-X",
+                             "tBodyGyro-mean()-Y",
+                             "tBodyGyro-mean()-Z",
+                             "tBodyGyro-std()-X",
+                             "tBodyGyro-std()-Y",
+                             "tBodyGyro-std()-Z",
+                             "tBodyGyroJerk-mean()-X",
+                             "tBodyGyroJerk-mean()-Y",
+                             "tBodyGyroJerk-mean()-Z",
+                             "tBodyGyroJerk-std()-X",
+                             "tBodyGyroJerk-std()-Y",
+                             "tBodyGyroJerk-std()-Z",
+                             "tBodyAccMag-mean()",
+                             "tBodyAccMag-std()",
+                             "tGravityAccMag-mean()",
+                             "tGravityAccMag-std()",
+                             "tBodyAccJerkMag-mean()",
+                             "tBodyAccJerkMag-std()",
+                             "tBodyGyroMag-mean()",
+                             "tBodyGyroMag-std()",
+                             "tBodyGyroJerkMag-mean()",
+                             "tBodyGyroJerkMag-std()",
+                             "fBodyAcc-mean()-X",
+                             "fBodyAcc-mean()-Y",
+                             "fBodyAcc-mean()-Z",
+                             "fBodyAcc-std()-X",
+                             "fBodyAcc-std()-Y",
+                             "fBodyAcc-std()-Z",
+                             "fBodyAccJerk-mean()-X",
+                             "fBodyAccJerk-mean()-Y",
+                             "fBodyAccJerk-mean()-Z",
+                             "fBodyAccJerk-std()-X",
+                             "fBodyAccJerk-std()-Y",
+                             "fBodyAccJerk-std()-Z",
+                             "fBodyGyro-mean()-X",
+                             "fBodyGyro-mean()-Y",
+                             "fBodyGyro-mean()-Z",
+                             "fBodyGyro-std()-X",
+                             "fBodyGyro-std()-Y",
+                             "fBodyGyro-std()-Z",
+                             "fBodyAccMag-mean()",
+                             "fBodyAccMag-std()",
+                             "fBodyBodyAccJerkMag-mean()",
+                             "fBodyBodyAccJerkMag-std()",
+                             "fBodyBodyGyroMag-mean()",
+                             "fBodyBodyGyroMag-std()",
+                             "fBodyBodyGyroJerkMag-mean()",
+                             "fBodyBodyGyroJerkMag-std()"
   )
   
   
   # remove all the temporary variables - likely not necessary but I did it during debugging
   rm(merged_set, test_activities, test_combined, 
-                 test_data, test_interim, test_subjects,
-                 training_activities, training_combined, training_data,
-                 training_interim, training_subjects)
+     test_data, test_interim, test_subjects,
+     training_activities, training_combined, training_data,
+     training_interim, training_subjects)
   
   # this call uses dplyr
   tidy_set <- trimmed_set %>% group_by(subject,activity) %>% summarise_each(funs(mean))
-
+  
   tidy_set
 }
 
-tidy_set <- run_analysis()
+
+test1 <- require(utils, quietly=TRUE)
+test2 <- require(dplyr, quietly=TRUE)
+
+if (test1 == FALSE) {
+  stop("Library: utils is not found.  Please install.packages(\"utils\") and try again.")
+} else if (test2 == FALSE) {
+  stop("Library: dplyr is not found.  Please install.packages(\"dplyr\") and try again.")
+} else {
+  tidy_set <- run_analysis()
+}
